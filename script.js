f71ea617f640211fa28a79567a1df3454564be93
The Gaf <bryangaffin@gmail.com>
2025-05-07 19:24:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,20 +5,8 @@ const notificationSound = new Audio("https://gaf.nyc/gafPing.mp3");
 let lastFirstUri = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("app");
   const fallback = document.getElementById("fallbackImage");
   if (fallback) fallback.remove();
-
-  container.innerHTML = `
-    <h2>🌥️ BSKY FEED VIEWER 🌥️</h2>
-    <input type="text" id="handleInput" placeholder="Enter handle (no @)" />
-    <button onclick="loadFeed()">Load Feed</button>
-    <div class="feed-box" id="feedContainer"></div>
-    <div class="nav-buttons">
-      <button onclick="changePage(-1)">Back</button>
-      <button onclick="changePage(1)">Next</button>
-    </div>
-  `;
 });
 
 async function loadFeed(playSound = false) {
